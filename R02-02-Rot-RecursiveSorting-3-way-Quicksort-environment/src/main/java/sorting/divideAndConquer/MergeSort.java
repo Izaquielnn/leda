ecb1599d746661dd74cba211c461f1368026e2ae
Izaquiel <izaquiel.nascimento@ccc.ufcg.edu.br>
2018-05-09 00:27:33 -0300

    Implementação MergeSort

--- R02-02-Rot-RecursiveSorting-3-way-Quicksort-environment/src/main/java/sorting/divideAndConquer/MergeSort.java
+++ R02-02-Rot-RecursiveSorting-3-way-Quicksort-environment/src/main/java/sorting/divideAndConquer/MergeSort.java
@@ -12,7 +12,54 @@ public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		
+		boolean valid = (leftIndex < rightIndex)    &&
+						 (array != null)            &&
+						 (leftIndex >= 0)            &&
+						 (rightIndex < array.length) &&
+						 (array.length > 0);
+		
+		if ( valid ) {
+			int mid = (leftIndex + rightIndex) / 2;
+			sort(array, leftIndex, mid);
+			sort(array, mid + 1, rightIndex);
+			merge(array, leftIndex, mid, rightIndex);
+		}
+	}
+
+	private void merge(T[] array, int leftIndex, int mid, int rightIndex) {
+		T[] aux = (T[]) new Comparable[array.length];  
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			aux[i] = array[i];
+		}
+		
+		int i = leftIndex;
+		int j = mid + 1;
+		int k = leftIndex;
+		
+		while (i <= mid && j <= rightIndex) {
+			if (aux[i].compareTo(aux[j]) < 0) {
+				array[k] = aux[i];
+				i += 1;
+			}else {
+				array[k] = aux[j];
+				j += 1;
+			}
+			
+			k += 1;
+		}
+		
+		while (i <= mid) {
+			array[k] = aux[i];
+			i += 1;
+			k += 1;
+		}
+		
+		while (j <= rightIndex) {
+			array[k] = aux[j];
+			j += 1;
+			k += 1;
+		}
+		
 	}
 }
